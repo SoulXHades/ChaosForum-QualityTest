ddccc1882720e3bb18884fe537dd44716ff4d216
SoulX <haojie_34@hotmail.com>
2020-03-17 14:20:09 +0800

    ASE
    1. Migrated navigateToSelectiveThread() and writeAPost() to Automate.py from PostTest.py

--- Automate.py
+++ Automate.py
@@ -17,3 +17,22 @@ def login(driver, isMod):
     password = driver.find_element_by_name("password")
     password.send_keys(pw)
     password.send_keys(Keys.RETURN)
+
+
+###################################### Post Related ######################################
+
+# navigate into the top thread from the list of threads on the home page
+def navigateToSelectiveThread(driver, threadIndex):
+	# delay 5 sec to let page load
+	driver.implicitly_wait(7)
+	# to enter the first thread on the list of threads in the home page
+	thread = driver.find_elements_by_class_name("thread-card.card")[threadIndex]
+	thread.click()
+
+# to post a content on a thread
+def writeAPost(driver, postContent):
+	# write post content and click the post button to post
+	postTxtBox = driver.find_element_by_class_name("form-control")
+	postTxtBox.send_keys(postContent)
+	postButton = driver.find_element_by_class_name("btn.btn-info")
+	postButton.click()
--- PostTest.py
+++ PostTest.py
@@ -6,19 +6,6 @@ import unittest
 import Automate
 
 class PostTest(unittest.TestCase):
-	# def __init__(self, *args, **kwargs):
-	# 	super().__init__(*args, **kwargs)
-
-	# 	self.driver = webdriver.Firefox()
-	# 	self.driver.get("https://chaosforum-20b27.firebaseapp.com/#/home")
-
-	# # need set longer delay else test scripts execute without page fully loaded yet, causing elements not found errors
-	# # def setUp(self):
-	# # 	self.driver.implicitly_wait(5)
-
-	# def __del__(self):
-	# 	self.driver.close();
-
 	def setUp(self):
 		self.driver = webdriver.Firefox()
 		self.driver.implicitly_wait(10)
@@ -44,7 +31,7 @@ class PostTest(unittest.TestCase):
 			# click to logout
 			logoutButton.click()
 
-		self.navigateToSelectiveThread(0)
+		Automate.navigateToSelectiveThread(self.driver, 0)
 
 		# delay 5 sec to let page load
 		#self.driver.implicitly_wait(5)
@@ -55,11 +42,11 @@ class PostTest(unittest.TestCase):
 		Automate.login(self.driver, False)
 		# thread selected should not be too close to thread 0 (top thread as new post bump threads to the top.
 		# May cause different test case function to race condition the posts in the same thread
-		self.navigateToSelectiveThread(4)
+		Automate.navigateToSelectiveThread(self.driver, 4)
 
 		# write post content and click the post button to post
 		testMsg = "Testing 123"
-		self.writeAPost(testMsg)
+		Automate.writeAPost(self.driver, testMsg)
 
 		# delay 5 sec to let page update to show the new post before comparing the content
 		time.sleep(5)
@@ -70,7 +57,7 @@ class PostTest(unittest.TestCase):
 	def test_postWithoutContent(self):
 		Automate.login(self.driver, False)
 		# navigate to first thread on the homepage
-		self.navigateToSelectiveThread(0)
+		Automate.navigateToSelectiveThread(self.driver, 0)
 
 		# if can find button with class name "btn.btn-info.disabled", means the button is disabled since we didn't write any content
 		self.postButton = self.driver.find_element_by_class_name("btn.btn-info.disabled")
@@ -88,8 +75,8 @@ class PostTest(unittest.TestCase):
 		Automate.login(self.driver, False)
 		# thread selected should not be too close to thread 0 (top thread as new post bump threads to the top.
 		# May cause different test case function to race condition the posts in the same thread
-		self.navigateToSelectiveThread(4)
-		self.writeAPost("Testing testing")
+		Automate.navigateToSelectiveThread(self.driver, 4)
+		Automate.writeAPost(self.driver, "Testing testing")
 
 		# delay 5 sec to let page update to load the new post into the page
 		time.sleep(5)
@@ -115,41 +102,3 @@ class PostTest(unittest.TestCase):
 		# see if the test post is successfully created
 		latestPost = self.driver.find_elements_by_class_name("card-text")[0]
 		self.assertEqual(latestPost.text, newEditedPostContent)
-
-
-	###################################### Own Functions ######################################
-	# login into the forum
-	def login(self):
-		try:
-			# to check the status of the login button see if it is not login before proceeding
-			loginButtonStatus = self.driver.find_element_by_class_name("dropdown-toggle.nav-link")
-		except selenium.common.exceptions.NoSuchElementException as e:
-			# if enters exception block means never
-			# search for login button then click it to navigate to the login page
-			loginButton = self.driver.find_element_by_class_name("btn.btn-outline-info")
-			loginButton.click()
-
-			# delay 5 sec to let page load
-			self.driver.implicitly_wait(5)
-			# input username and password before logging in via pressing the ENTER button
-			usernameTxtBox = self.driver.find_elements_by_class_name("form-control")[0]
-			usernameTxtBox.send_keys("C170095@e.ntu.edu.sg")
-			passwdTxtBox = self.driver.find_elements_by_class_name("form-control")[1]
-			passwdTxtBox.send_keys("C170095@e.ntu.edu.sg")
-			passwdTxtBox.send_keys(Keys.ENTER)
-
-	# navigate into the top thread from the list of threads on the home page
-	def navigateToSelectiveThread(self, threadIndex):
-		# delay 5 sec to let page load
-		self.driver.implicitly_wait(7)
-		# to enter the first thread on the list of threads in the home page
-		thread = self.driver.find_elements_by_class_name("thread-card.card")[threadIndex]
-		thread.click()
-
-	# to post a content on a thread
-	def writeAPost(self, postContent):
-		# write post content and click the post button to post
-		postTxtBox = self.driver.find_element_by_class_name("form-control")
-		postTxtBox.send_keys(postContent)
-		postButton = self.driver.find_element_by_class_name("btn.btn-info")
-		postButton.click()
