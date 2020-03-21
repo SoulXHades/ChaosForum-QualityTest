89e1b1af3c29bb872a140f5abc3a0c6a3a311553
SoulX <haojie_34@hotmail.com>
2020-03-22 01:03:24 +0800

    ASE

--- ChaosWebTest_Main.py
+++ ChaosWebTest_Main.py
@@ -5,16 +5,13 @@ import HtmlTestRunner
 import os
 
 def main():
-    # suite = unittest.TestLoader().loadTestsFromModule(LoginTest)
-    # unittest.TextTestRunner(verbosity=2).run(suite)
-
     suite = unittest.TestSuite()
     tests = unittest.TestLoader()
-    # suite.addTests(tests.loadTestsFromModule(LoginTest))
+    suite.addTests(tests.loadTestsFromModule(LoginTest))
     suite.addTests(tests.loadTestsFromModule(ThreadTest))
-    # suite.addTests(tests.loadTestsFromModule(PostTest))
-    # suite.addTests(tests.loadTestsFromModule(RegistrationTest))
-    # unittest.TextTestRunner(verbosity=2).run(suite)
+    suite.addTests(tests.loadTestsFromModule(PostTest))
+    suite.addTests(tests.loadTestsFromModule(RegistrationTest))
+    unittest.TextTestRunner(verbosity=2).run(suite)
     
     
     curr_dir = os.getcwd()
